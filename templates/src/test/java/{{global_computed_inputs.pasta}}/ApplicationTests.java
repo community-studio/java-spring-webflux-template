5dd4281974b04ecebc3c481dcdb6c70a5bd32024
Maurício Gomes <alcides.filho@orangestack.com>
2022-07-13 10:59:03 -0300

    Update variables

--- templates/src/test/java/{{global_computed_inputs.pasta}}/ApplicationTests.java
+++ templates/src/test/java/{{global_computed_inputs.pasta}}/ApplicationTests.java
@@ -1,4 +1,4 @@
-package {{computed_inputs.pacote_base}};
+package {{global_computed_inputs.pacote_base}};
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
